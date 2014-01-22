f646d9ff8d70fa7c713dd9549b2ae2b709707d5d
gregory <greg2502@gmail.com>
2014-01-21 19:25:25 -0800

    add pieces on the board

--- lib/rchess/board.rb
+++ lib/rchess/board.rb
@@ -4,9 +4,33 @@ module Rchess
 
     def initialize
       self.boxes = []
+      initialize_boxes
+      initialize_pieces
+    end
 
-      (0..7).each do |x|
-        self.boxes << (0..7).map{ |y| Box.new(Integer("#{y}#{x}",10)) }
+    private
+
+    def initialize_boxes
+      (0..7).each do |y|
+        self.boxes << (0..7).map{ |x| Box.new(Integer("#{y}#{x}",10)) }
+      end
+    end
+
+    def initialize_pieces
+      below_line = self.boxes[0]
+      above_line = self.boxes[-1]
+
+      pieces = Rchess::PieceBuilder::PIECES.invert
+
+      below_line[0].host = below_line[-1].host = above_line[0].host = above_line[-1].host = pieces['rook']
+      below_line[1].host = below_line[-2].host = above_line[1].host = above_line[-2].host = pieces['knight']
+      below_line[2].host = below_line[-3].host = above_line[2].host = above_line[-3].host = pieces['bishop']
+
+      below_line[3].host = above_line[3].host = pieces['queen']
+      below_line[4].host = above_line[4].host = pieces['king']
+
+      (self.boxes[1] +self.boxes[-2]).each do |box|
+        box.host = pieces['pawn']
       end
     end
   end
---
+++ lib/rchess/piece_builder.rb
@@ -0,0 +1,12 @@
+module Rchess
+  class PieceBuilder
+    PIECES ={
+      p: 'pawn',
+      b: 'bishop',
+      q: 'queen',
+      c: 'knight',
+      r: 'rook',
+      k: 'king'
+    }
+  end
+end
