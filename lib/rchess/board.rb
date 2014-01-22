f74ac97cb17815fe278bb16d1aa4952a20ee81c9
gregory <greg2502@gmail.com>
2014-01-21 21:01:01 -0800

    add board, game and put some color on that

--- lib/rchess/board.rb
+++ lib/rchess/board.rb
@@ -1,19 +1,51 @@
 module Rchess
   class Board
+    BOUDARIES = (0..8)
+    COLORS = {
+      white: :downcase,
+      black: :upcase
+    }
+
     attr_accessor :boxes
 
     def initialize
       self.boxes = []
       initialize_boxes
       initialize_pieces
+      initialize_colors
+    end
+
+    def color_for_piece_at(atBox)
+      host = box_at_pos(atBox.pos).host
+      COLORS.detect{|k,v| host.send(v) == host}.first
+    end
+
+    def box_at_pos(pos)
+      self.boxes.flatten.detect{|box| box.pos == pos }
+    end
+
+    def box_from_coord(x, y)
+      box_from_pos(Integer("#{y}#{x}", 10))
+    end
+
+    def box_from_pos(pos)
+      Box.new(pos).tap do |box|
+        coord = box.to_coord
+        x, y  = coord[:x], coord[:y]
+        raise ArgumentError.new("Box out of the board") unless BOUDARIES.include?(x) && BOUDARIES.include?(y)
+      end
     end
 
     private
 
     def initialize_boxes
       (0..7).each do |y|
-        self.boxes << (0..7).map{ |x| Box.new(Integer("#{y}#{x}",10)) }
+        self.boxes << (0..7).map{ |x| box_from_coord(x, y) }
       end
+    end
+
+    def initialize_colors
+      (self.boxes[-1] + self.boxes[-2]).each{|box| box.host = box.host.upcase}
     end
 
     def initialize_pieces
--- lib/rchess/game.rb
+++ lib/rchess/game.rb
@@ -4,13 +4,22 @@ module Rchess
     COLORS = [:white, :black]
 
     attr_accessor :board
-    attr_reader :players
+    attr_reader :players, :turn
+
+    def initialize
+      @turn = :white
+    end
 
     def add_player(player, color)
       #TODO: validate that the color has not been picked
-      raise InvalidArgument.new("Unknown color - Choose among: #{COLORS.join(', ')}") unless COLORS.include? color
+      raise ArgumentError.new("Unknown color - Choose among: #{COLORS.join(', ')}") unless COLORS.include? color
 
       players[player.uuid] = {color: color, player: player}
+    end
+
+    def player_moved(player, fromBox, toBox)
+      publish(:you_are_frozen) unless self.player_can_play?(player)
+      publish(:you_occupy_this_position) unless self.player_dont_own_the_box?(player, toBox)
     end
 
     def players
@@ -19,6 +28,14 @@ module Rchess
 
     def board
       @board ||= Board.new
+    end
+
+    def player_can_play?(player)
+      self.players[player.uuid][:color] == self.turn
+    end
+
+    def player_own_box?(player, box)
+      self.board.color_for_piece_at(box) == players[player.uuid][:color]
     end
   end
 end
