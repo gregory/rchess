aa0f2659fe70c7b6ff701ed64f1716dfd4df56c3
gregory <greg2502@gmail.com>
2014-01-23 22:08:55 -0800

    add piece design

--- lib/rchess/piece.rb
+++ lib/rchess/piece.rb
@@ -1,13 +1,35 @@
 module Rchess
   class Piece
-    attr_reader :box, :board, :color
-    attr_accessor :destinations
+    TYPES={
+      p: 'pawn',
+      b: 'bishop',
+      q: 'queen',
+      c: 'knight',
+      r: 'rook',
+      k: 'king'
+    }
 
-    def initialize(box, board)
-      @box = box
-      @board = board
-      @destinations = []
-      @color = board.color_for_piece_at(box.pos)
+    COLORS={
+      white: :downcase,
+      black: :upcase
+    }
+
+    attr_accessor :coord, :type
+
+    def initialize(options)
+      self.type  = options.fetch(:type, TYPES.invert['pawn'])
+      self.coord = options.fetch(:coord)
+    end
+
+    def type=(value)
+      raise ArgumentError.new("Unknown type #{value}") unless TYPES.keys.include?(value.downcase)
+      @type = value
+    end
+
+    def color
+      return @color if @color
+      cases = COLORS.invert
+      @color = self.type == self.type.downcase ? cases[:downcase] : cases[:upcase]
     end
   end
 end
