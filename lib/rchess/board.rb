1eb99a8b4cea3ab58bdb2530d16a937382a0ad60
gregory <greg2502@gmail.com>
2014-01-23 22:08:03 -0800

    pre move game validation

--- lib/rchess/board.rb
+++ lib/rchess/board.rb
@@ -10,5 +10,22 @@ module Rchess
     def boxes
       @boxes ||= Array.new(BOUDARIES.count){ Array.new(BOUDARIES.count){ EMPTY_BOX }}
     end
+
+    def coord_within_boundaries?(coord)
+      BOUDARIES.include?(coord.x) && BOUDARIES.include?(coord.y)
+    end
+
+    def movement_within_board?(srcCoord, dstCoord)
+      coord_within_boundaries?(srcCoord) || coord_within_boundaries?(dstCoord)
+    end
+
+    def piece_color_at_coord(coord)
+      self.piece_at_coord(coord).color
+    end
+
+    def piece_at_coord(coord)
+      piece_type = self.boxes[coord.y][coord.x]
+      Piece.new({type: piece_type, coord: coord})
+    end
   end
 end
--- lib/rchess/game.rb
+++ lib/rchess/game.rb
@@ -1,18 +1,29 @@
 module Rchess
   class Game
     attr_accessor :board, :players
-    attr_reader :current_player, :check_player
+    attr_reader :current_player_color
 
     def initialize(players)
-      self.players         = players
-      @current_player  = :white
-      @player_in_check = nil
+      self.players          = players
+      @current_player_color = :white
     end
 
     def players=(value)
       @players = {}
-      @players[:white] = value.fetch(:white){ raise ArgumentError.new("Please provide a white player")}
-      @players[:black] = value.fetch(:black){ raise ArgumentError.new("Please provide a black player")}
+      player_white = value.fetch(:white){ raise ArgumentError.new("Please provide a white player")}
+      player_black = value.fetch(:black){ raise ArgumentError.new("Please provide a black player")}
+      @players[player_black.uuid] = { color: :black, player: player_black }
+      @players[player_white.uuid] = { color: :white, player: player_white }
+    end
+
+    def move(srcCoord, dstCoord)
+      return false unless board.movement_within_board?(srcCoord, dstCoord)
+      return false unless current_player_own_piece_at_coord?(srcCoord)
+      #return false unless board.valid_move(srcCoord, dstCoord)
+    end
+
+    def current_player_own_piece_at_coord?(coord)
+      board.piece_color_at_coord(coord) == current_player_color
     end
 
     def board
